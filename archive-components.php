b03cbcedb99a68335baab18b58037fe11f171546
brianxautumn <shred@brianautumn.com>
2016-07-21 13:36:42 -0700

    Added Custom Component Type support

---
+++ archive-components.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+
+
+
+
+
+    <header class="entry-header">
+        <div class="container">
+            <div class="col-xs-12">
+                <h1>Components</h1>
+            </div>
+        </div>
+    </header>
+
+
+
+
+<?php get_footer(); ?> 
--- functions.php
+++ functions.php
@@ -12,61 +12,128 @@ if (!function_exists('flare-twentysixteen_setup')) :
         ));
     }
 
-
-    
 endif;
-add_action( 'after_setup_theme', 'flare_twentysixteen_setup' );
+add_action('after_setup_theme', 'flare_twentysixteen_setup');
 
+function flare_twentysixteen_component_init() {
+    $labels = array(
+        'name' => _x('Components', 'post type general name', 'flare_twentysixteen'),
+        'singular_name' => _x('Component', 'post type singular name', 'flare_twentysixteen'),
+        'menu_name' => _x('Components', 'admin menu', 'flare_twentysixteen'),
+        'name_admin_bar' => _x('Component', 'add new on admin bar', 'flare_twentysixteen'),
+        'add_new' => _x('Add New', 'component', 'flare_twentysixteen'),
+        'add_new_item' => __('Add New Component', 'flare_twentysixteen'),
+        'new_item' => __('New Component', 'flare_twentysixteen'),
+        'edit_item' => __('Edit Component', 'flare_twentysixteen'),
+        'view_item' => __('View Component', 'flare_twentysixteen'),
+        'all_items' => __('All Components', 'flare_twentysixteen'),
+        'search_items' => __('Search Components', 'flare_twentysixteen'),
+        'parent_item_colon' => __('Parent Components:', 'flare_twentysixteen'),
+        'not_found' => __('No components found.', 'flare_twentysixteen'),
+        'not_found_in_trash' => __('No components found in Trash.', 'flare_twentysixteen')
+    );
 
-function flare_twentysixteen_componenet_init() {
-	$labels = array(
-		'name'               => _x( 'Componenets', 'post type general name', 'flare_twentysixteen' ),
-		'singular_name'      => _x( 'Componenet', 'post type singular name', 'flare_twentysixteen' ),
-		'menu_name'          => _x( 'Componenets', 'admin menu', 'flare_twentysixteen' ),
-		'name_admin_bar'     => _x( 'Componenet', 'add new on admin bar', 'flare_twentysixteen' ),
-		'add_new'            => _x( 'Add New', 'componenet', 'flare_twentysixteen' ),
-		'add_new_item'       => __( 'Add New Componenet', 'flare_twentysixteen' ),
-		'new_item'           => __( 'New Componenet', 'flare_twentysixteen' ),
-		'edit_item'          => __( 'Edit Componenet', 'flare_twentysixteen' ),
-		'view_item'          => __( 'View Componenet', 'flare_twentysixteen' ),
-		'all_items'          => __( 'All Componenets', 'flare_twentysixteen' ),
-		'search_items'       => __( 'Search Componenets', 'flare_twentysixteen' ),
-		'parent_item_colon'  => __( 'Parent Componenets:', 'flare_twentysixteen' ),
-		'not_found'          => __( 'No componenets found.', 'flare_twentysixteen' ),
-		'not_found_in_trash' => __( 'No componenets found in Trash.', 'flare_twentysixteen' )
-	);
+    $args = array(
+        'labels' => $labels,
+        'description' => __('Description.', 'flare_twentysixteen'),
+        'public' => true,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'query_var' => true,
+        'rewrite' => array('slug' => 'components'),
+        'capability_type' => 'page',
+        'has_archive' => true,
+        'hierarchical' => true,
+        'menu_position' => null,
+        'supports' => array('title', 'editor', 'excerpt'),
+        'taxonomies' => array('component-category', 'component-tag'),
+        'menu_icon' => 'dashicons-admin-generic'
+    );
 
-	$args = array(
-		'labels'             => $labels,
-                'description'        => __( 'Description.', 'flare_twentysixteen' ),
-		'public'             => true,
-		'publicly_queryable' => true,
-		'show_ui'            => true,
-		'show_in_menu'       => true,
-		'query_var'          => true,
-		'rewrite'            => array( 'slug' => 'componenet' ),
-		'capability_type'    => 'page',
-		'has_archive'        => true,
-		'hierarchical'       => false,
-		'menu_position'      => null,
-		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
-	);
-
-	register_post_type( 'componenet', $args );
+    register_post_type('components', $args);
 }
 
-add_action( 'init', 'flare_twentysixteen_componenet_init' );
+add_action('init', 'flare_twentysixteen_component_init');
+
+
+
+
+
+add_action('init', 'flare_twentysixteen_component_taxonomies', 0);
+
+// create two taxonomies, genres and writers for the post type "book"
+function flare_twentysixteen_component_taxonomies() {
+    // Add new taxonomy, make it hierarchical (like categories)
+    $labels = array(
+        'name' => _x('Category', 'taxonomy general name'),
+        'singular_name' => _x('Category', 'taxonomy singular name'),
+        'search_items' => __('Search Categories'),
+        'all_items' => __('All Categories'),
+        'parent_item' => __('Parent Category'),
+        'parent_item_colon' => __('Parent Category:'),
+        'edit_item' => __('Edit Category'),
+        'update_item' => __('Update Category'),
+        'add_new_item' => __('Add New Category'),
+        'new_item_name' => __('New Category Name'),
+        'menu_name' => __('Category'),
+    );
+
+    $args = array(
+        'hierarchical' => true,
+        'labels' => $labels,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'query_var' => true,
+        'rewrite' => array('slug' => 'component-category'),
+    );
+
+    register_taxonomy('component-category', array('components'), $args);
+
+    // Add new taxonomy, NOT hierarchical (like tags)
+    $labels = array(
+        'name' => _x('Tags', 'taxonomy general name'),
+        'singular_name' => _x('Tag', 'taxonomy singular name'),
+        'search_items' => __('Search Tags'),
+        'popular_items' => __('Popular Tags'),
+        'all_items' => __('All Tags'),
+        'parent_item' => null,
+        'parent_item_colon' => null,
+        'edit_item' => __('Edit Tag'),
+        'update_item' => __('Update Tag'),
+        'add_new_item' => __('Add New Tag'),
+        'new_item_name' => __('New Tag'),
+        'separate_items_with_commas' => __('Separate tags with commas'),
+        'add_or_remove_items' => __('Add or remove tags'),
+        'choose_from_most_used' => __('Choose from the most used tags'),
+        'not_found' => __('No tags found.'),
+        'menu_name' => __('Tags'),
+    );
+
+    $args = array(
+        'hierarchical' => false,
+        'labels' => $labels,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'update_count_callback' => '_update_post_term_count',
+        'query_var' => true,
+        'rewrite' => array('slug' => 'components-tag'),
+    );
+
+    register_taxonomy('component-tag', 'components', $args);
+}
 
 function flare_twentysixteen_scripts() {
-    wp_enqueue_style( 'fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' );
-    wp_enqueue_style( 'fontawesome', get_template_directory_uri() . '/css/font-awesome.min.css' );
-    wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css' );
-    wp_enqueue_style( 'highlight-css', get_template_directory_uri() . '/plugins/highlight/styles/default.css' );
-    wp_enqueue_style( 'flare-twentysixteen-style', get_stylesheet_uri() );
-    
+    wp_enqueue_style('fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800');
+    wp_enqueue_style('fontawesome', get_template_directory_uri() . '/css/font-awesome.min.css');
+    wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css');
+    wp_enqueue_style('highlight-css', get_template_directory_uri() . '/plugins/highlight/styles/default.css');
+    wp_enqueue_style('flare-twentysixteen-style', get_stylesheet_uri());
+
     wp_enqueue_script('jquery');
-    wp_enqueue_script('bootstrap-js' , get_template_directory_uri() . '/js/bootstrap.min.js');
-    wp_enqueue_script('highlight-js' ,get_template_directory_uri() . '/plugins/highlight/highlight.pack.js' );
-    wp_enqueue_script('flare-main-js' ,get_template_directory_uri() .'/js/main.js' );
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js');
+    wp_enqueue_script('highlight-js', get_template_directory_uri() . '/plugins/highlight/highlight.pack.js');
+    wp_enqueue_script('flare-main-js', get_template_directory_uri() . '/js/main.js');
 }
-add_action( 'wp_enqueue_scripts', 'flare_twentysixteen_scripts' );
+
+add_action('wp_enqueue_scripts', 'flare_twentysixteen_scripts');
