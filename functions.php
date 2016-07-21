3eb5c13d3968cd1a4e8ef7a9ab0740db9e2c5935
brianxautumn <shred@brianautumn.com>
2016-07-21 11:38:44 -0700

    Added custom componenet page type

--- functions.php
+++ functions.php
@@ -18,6 +18,45 @@ endif;
 add_action( 'after_setup_theme', 'flare_twentysixteen_setup' );
 
 
+function flare_twentysixteen_componenet_init() {
+	$labels = array(
+		'name'               => _x( 'Componenets', 'post type general name', 'flare_twentysixteen' ),
+		'singular_name'      => _x( 'Componenet', 'post type singular name', 'flare_twentysixteen' ),
+		'menu_name'          => _x( 'Componenets', 'admin menu', 'flare_twentysixteen' ),
+		'name_admin_bar'     => _x( 'Componenet', 'add new on admin bar', 'flare_twentysixteen' ),
+		'add_new'            => _x( 'Add New', 'componenet', 'flare_twentysixteen' ),
+		'add_new_item'       => __( 'Add New Componenet', 'flare_twentysixteen' ),
+		'new_item'           => __( 'New Componenet', 'flare_twentysixteen' ),
+		'edit_item'          => __( 'Edit Componenet', 'flare_twentysixteen' ),
+		'view_item'          => __( 'View Componenet', 'flare_twentysixteen' ),
+		'all_items'          => __( 'All Componenets', 'flare_twentysixteen' ),
+		'search_items'       => __( 'Search Componenets', 'flare_twentysixteen' ),
+		'parent_item_colon'  => __( 'Parent Componenets:', 'flare_twentysixteen' ),
+		'not_found'          => __( 'No componenets found.', 'flare_twentysixteen' ),
+		'not_found_in_trash' => __( 'No componenets found in Trash.', 'flare_twentysixteen' )
+	);
+
+	$args = array(
+		'labels'             => $labels,
+                'description'        => __( 'Description.', 'flare_twentysixteen' ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'componenet' ),
+		'capability_type'    => 'page',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+	);
+
+	register_post_type( 'componenet', $args );
+}
+
+add_action( 'init', 'flare_twentysixteen_componenet_init' );
+
 function flare_twentysixteen_scripts() {
     wp_enqueue_style( 'fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' );
     wp_enqueue_style( 'fontawesome', get_template_directory_uri() . '/css/font-awesome.min.css' );
